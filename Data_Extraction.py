d09a2ac7a5ace02920a62a59fcf5a160ce5e47c1
revanthjavvaji <114976742+revanthjavvaji@users.noreply.github.com>
2023-01-05 19:36:01 +0530

    Add files via upload

---
+++ Data_Extraction.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class BlackcofferspiderSpider(scrapy.Spider):
+    name = 'BlackcofferSpider'
+    allowed_domains = ['example.com']
+    start_urls = ['http://example.com/']
+
+    def parse(self, response):
+        pass
